57613cedf0a1668785ddda55913138c2129cf362
unknown <23040456@student.ltuc.com>
2026-02-02 23:38:58 +0300

    abc

--- js/app.js
+++ js/app.js
@@ -2,14 +2,61 @@
 
 let mobileMenu = document.querySelector(".header .container .inside .right");
 let menuIcon = document.querySelector(".header .container .inside .menu-icon");
+let flipSwitch = document.querySelector("input[type='checkbox']#fs");
+//
+let translations = {};
+//
+
+// Start Languages Functions ---- START
+async function loadLang(lang) {
+  const res = await fetch(`lang/${lang}.json`);
+  translations = await res.json();
+
+  document.documentElement.lang = lang;
+  document.documentElement.dir = lang === "ar" ? "rtl" : "ltr";
+
+  applyTranslations();
+}
+
+function getValue(obj, key) {
+  return key.split(".").reduce((o, i) => o[i], obj);
+}
+
+function applyTranslations() {
+  document.querySelectorAll("[data-i18n]").forEach((el) => {
+    el.innerHTML = getValue(translations, el.dataset.i18n);
+  });
+
+  document.querySelectorAll("[data-i18n-placeholder]").forEach((el) => {
+    el.placeholder = getValue(translations, el.dataset.i18nPlaceholder);
+  });
+}
+// End Languages Functions   ---- END
+
+// Fetch Json Data
 
 //
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadLang("ar");
+  flipSwitch.checked = true;
+});
+
 menuIcon.addEventListener("click", (event) => {
   mobileMenu.classList.toggle("show-menu");
+  // Try Lang Toggle
 });
 
 addEventListener("resize", () => {
   if (window.innerWidth > 500) {
     mobileMenu.classList.remove("show-menu");
+  }
+});
+
+flipSwitch.addEventListener("change", () => {
+  if (flipSwitch.checked) {
+    loadLang("ar");
+  } else {
+    loadLang("en");
   }
 });
